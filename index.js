9696fe6dcc1ef7ab4966d9148a33f90128129567
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-13 21:00:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ function multiply(a, b){
   var solution = a * b 
   return solution
 }
-var a, b
+/*var a, b
 
 beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
-})
+})*/
 
 function divide(a, b){
   var solution = a / b 
