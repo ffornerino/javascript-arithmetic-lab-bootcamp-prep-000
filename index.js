533d92d52312c47f55e2ddbecdb2715a3443f312
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-13 20:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function add(n){
-  var solution = a +=b 
+  var solution = a + b 
   return solution
-  
+}
+
+function subtract(n){
+  var solution = a - b 
+  return solution
+}
+
+function multiply(n){
+  var solution = a * b 
+  return solution
+}
+
+function divide(n){
+  var solution = a / b 
+  return solution
 }
