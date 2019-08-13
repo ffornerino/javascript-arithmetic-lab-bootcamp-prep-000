fc2116c791a0645ab30e5956bcb39802b155330d
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-13 20:57:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
 function add(n){
   var solution = a + b 
   return solution
 }
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
 
 function subtract(n){
   var solution = a - b 
   return solution
 }
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
 
 function multiply(n){
   var solution = a * b 
   return solution
 }
+var a, b
+
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
 
 function divide(n){
   var solution = a / b 
